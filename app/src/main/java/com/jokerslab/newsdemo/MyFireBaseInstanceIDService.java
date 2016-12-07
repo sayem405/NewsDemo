4b519b291a0a51d664b1f2eb372b21045acf3332
Sayem <sayem.cse18@gmail.com>
2016-12-07 17:03:08 +0600

    firebase basic setup added

---
+++ app/src/main/java/com/jokerslab/newsdemo/MyFireBaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package com.jokerslab.newsdemo;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by sayem on 12/7/16.
+ */
+
+public class MyFireBaseInstanceIDService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/jokerslab/newsdemo/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.jokerslab.newsdemo;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by sayem on 12/7/16.
+ */
+
+public class MyFireBaseMessagingService extends FirebaseMessagingService {
+}
